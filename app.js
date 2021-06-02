9607a4792806407cdf90bb67ad4e4ac40afbca8a
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-06-02 19:29:32 +0530

    limit
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 app.use(morgan("dev"));
 
@@ -22,6 +22,7 @@ app.use("/api/v1/orders", require("./src/api/v1/orders/routes"));
 app.use("/api/v1/admin", require("./src/api/v1/admin/routes"));
 
 app.use((err, req, res, next) => {
+  console.log(err);
   return res.status(500).json({
     ok: false,
     error: err.message,
