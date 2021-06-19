4cc441324104eb80a47cb1f1e1410f58b0cbcff3
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-06-19 21:19:51 +0530

    Fixed senstive case bug
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- src/api/v1/users/models/user.entity.js
+++ src/api/v1/users/models/user.entity.js
@@ -7,11 +7,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       minLength: 4,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
       validate:
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     },
